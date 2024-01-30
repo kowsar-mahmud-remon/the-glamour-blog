1c2ec41a91ee4b50207173eaa5a5c021a02b4605
creativerze <creativerze@gmail.com>
2024-01-30 08:41:04 +0600

    added redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import store from "@/redux/store";
 import "@/styles/globals.css";
+import { Provider } from "react-redux";
 
 export default function MyApp({ Component, pageProps }: any) {
   const getLayout = Component.getLayout || ((page: any) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
