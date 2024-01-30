2183fc2c26d340f9d9e5ec2327f9c73ccf11863b
creativerze <creativerze@gmail.com>
2024-01-30 09:13:37 +0600

    added blogApi

---
+++ src/redux/features/blog/blogApi.ts
@@ -0,0 +1,50 @@
+import { api } from "../../api/apiSlice";
+
+const bikeApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    getAllBlogs: build.query({
+      query: () => "/blog",
+      providesTags: ["blog"],
+    }),
+
+    getSingleBlog: build.query({
+      query: (id: any) => `/blog/${id}`,
+      providesTags: ["blog"],
+    }),
+
+    createBlog: build.mutation({
+      query: (data: any) => ({
+        url: `/blog/create-blog`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["blog"],
+    }),
+
+    deleteBlog: build.mutation({
+      query: (id: any) => ({
+        url: `/blog/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
+      invalidatesTags: ["blog"],
+    }),
+
+    updateBlog: build.mutation({
+      query: ({ id, data }: any) => ({
+        url: `/blog/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: ["blog"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllBikesQuery,
+  useGetSingleBikeQuery,
+  useCreateBikeMutation,
+  useDeleteBikeMutation,
+  useUpdateBikeMutation,
+} = bikeApi;
