0503ec91c3c31076df02e09daf53382126ae657d
creativerze <creativerze@gmail.com>
2024-01-30 08:34:22 +0600

    added home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import RootLayout from "@/components/Layout/RootLayout";
+import HomePage from "@/components/Home/Home";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
 }
+
+Home.getLayout = function getLayout(page: any) {
+  return <RootLayout>{page}</RootLayout>;
+};
